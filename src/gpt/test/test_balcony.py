19eb5d7d13aae6c168bdf0cab3f7256442406fbc
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2023-09-25 15:57:49 +0200

    Creating GPT evaluation

--- src/gpt/test/test_balcony.py
+++ src/gpt/test/test_balcony.py
@@ -9,28 +9,22 @@ import translators as ts
 import requests
 from contextlib import suppress
 
-def test_balcony_gpt():
-    balcony_chain = [
-        balcony_summary_chain, balcony_rating_chain,
-    ]
 
-    balcony_output_variables = [
-        "balcony_summary", "balcony_rating",
-    ]
-
+def test_gpt(chain: list, output_vars: list[str], input_data_json: str):
     # Load data
-    with open('./src/gpt/data/test-data/test-balcony.json', mode='r', encoding='utf-8') as file:
-        balcony_data = json.load(file)
 
-    for offer_record in balcony_data[1:2]:
+    with open(f'./src/gpt/test/test-data/{input_data_json}', mode='r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    for offer_record in data[1:2]:
         offer_description = offer_record['description']
 
         offer_description_en = ts.translate_text(offer_description, translator='baidu', to_language='en')
 
         overall_chain = SequentialChain(
-            chains=balcony_chain,
+            chains=chain,
             input_variables=['real_estate_offer_en'],
-            output_variables=balcony_output_variables,
+            output_variables=output_vars,
             verbose=True,
         )
 
